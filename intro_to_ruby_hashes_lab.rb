5aba0980cade21579a828a8657208bcc003eaa37
Michael Quigley <mikequigley@quigleypublishing.com>
2020-01-12 03:49:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
 	hash = {:railroads => {
 		        :pieces => 4,
 						:rent_in_dollars => {
-							:one_piece_owned => "25",
+							:one_piece_owned => 25,
 							:key2 => "2",
 							:key3 => "3",
 							:key4 => "4"
